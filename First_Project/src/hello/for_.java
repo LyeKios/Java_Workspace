c3ae8e9a397a0b82c2d303fbb67f37925260e6eb
LyeKios <2546216709@qq.com>
2020-09-16 21:38:23 +0800

    new 9.16

---
+++ First_Project/src/hello/for_.java
@@ -0,0 +1,12 @@
+package hello;
+
+public class for_ {
+	public static void main(String[] args) {
+		String[] sentences = {"hello", "thank u", "thank u very much"};
+		for (String item : sentences) {
+		    System.out.println(item);
+		}
+	}
+		
+
+}
